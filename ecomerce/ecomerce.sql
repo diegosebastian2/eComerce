fd990ea0ab1b90cc07ee692923ef29673e66e7ac
diegosebastian2 <diegosebastian2@gmail.com>
2018-04-07 14:21:16 -0300

    base update

--- ecomerce/ecomerce.sql
+++ ecomerce/ecomerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-04-2018 a las 14:33:13
+-- Tiempo de generación: 07-04-2018 a las 19:18:22
 -- Versión del servidor: 10.1.30-MariaDB
 -- Versión de PHP: 7.2.2
 
@@ -47,6 +47,17 @@ CREATE TABLE `categorias` (
   `habilitado` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `habilitado`) VALUES
+(1, 'Televisores', 1),
+(2, 'Notebooks', 1),
+(3, 'Lavarropas', 1),
+(4, 'Consolas', 1),
+(6, 'Heladeras', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -60,7 +71,7 @@ CREATE TABLE `compras` (
   `producto_id` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL,
-  `operaciones_compras_id` int(11) NOT NULL
+  `estado_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -89,7 +100,8 @@ CREATE TABLE `productos` (
   `precio` decimal(10,0) NOT NULL,
   `precio_descuento` decimal(10,0) NOT NULL,
   `categoria_id` int(11) NOT NULL,
-  `habilitado` tinyint(1) NOT NULL DEFAULT '1'
+  `habilitado` tinyint(1) NOT NULL DEFAULT '1',
+  `estado_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -104,6 +116,14 @@ CREATE TABLE `productos_estados` (
   `habilitado` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `productos_estados`
+--
+
+INSERT INTO `productos_estados` (`id`, `estado`, `habilitado`) VALUES
+(1, 'En Stock', 1),
+(2, 'Sin Stock', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -117,6 +137,17 @@ CREATE TABLE `usuarios` (
   `habilitado` tinyint(1) DEFAULT '1',
   `nombre` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `email`, `password`, `habilitado`, `nombre`) VALUES
+(1, 'usuario1@mail.com', 'password1', 1, 'andres'),
+(2, 'usuario2@mail.com', 'password2', 1, 'sofia'),
+(3, 'usuario3@mail.com', 'password3', 1, 'veronica'),
+(4, 'usuario4@mail.com', 'password4', 1, 'julio'),
+(5, 'usuario5@mail.com', 'password5', 1, 'alejandro');
 
 --
 -- Índices para tablas volcadas
@@ -138,7 +169,8 @@ ALTER TABLE `categorias`
 -- Indices de la tabla `compras`
 --
 ALTER TABLE `compras`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `estado_id` (`estado_id`);
 
 --
 -- Indices de la tabla `compras_estados`
@@ -151,12 +183,14 @@ ALTER TABLE `compras_estados`
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `categoria_id` (`categoria_id`);
+  ADD KEY `categoria_id` (`categoria_id`),
+  ADD KEY `estado_id` (`estado_id`);
 
 --
 -- Indices de la tabla `productos_estados`
 --
 ALTER TABLE `productos_estados`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`);
 
 --
@@ -166,26 +200,43 @@ ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `productos_estados`
+--
+ALTER TABLE `productos_estados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `categorias`
+-- Filtros para la tabla `compras`
 --
-ALTER TABLE `categorias`
-  ADD CONSTRAINT `categorias_ibfk_1` FOREIGN KEY (`id`) REFERENCES `productos` (`categoria_id`);
+ALTER TABLE `compras`
+  ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`estado_id`) REFERENCES `compras_estados` (`id`);
 
 --
--- Filtros para la tabla `compras_estados`
+-- Filtros para la tabla `productos`
 --
-ALTER TABLE `compras_estados`
-  ADD CONSTRAINT `compras_estados_ibfk_1` FOREIGN KEY (`id`) REFERENCES `compras` (`id`);
-
---
--- Filtros para la tabla `productos_estados`
---
-ALTER TABLE `productos_estados`
-  ADD CONSTRAINT `productos_estados_ibfk_1` FOREIGN KEY (`id`) REFERENCES `productos` (`id`);
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`),
+  ADD CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`estado_id`) REFERENCES `productos_estados` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
