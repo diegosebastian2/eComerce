39a3fd7bd3f242d820bd4616beba7ba60687a6f4
diegosebastian2 <diegosebastian2@gmail.com>
2018-04-09 17:58:44 -0300

    pull prueba

--- ecomerce/ecomerce.sql
+++ ecomerce/ecomerce.sql
@@ -1,244 +1,102 @@
--- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 07-04-2018 a las 19:18:22
--- Versión del servidor: 10.1.30-MariaDB
--- Versión de PHP: 7.2.2
+INSERT INTO `productos`(
+    `nombre`,
+    `descuento`,
+    `codigo`,
+    `precio`,
+    `precio_descuento`,
+    `categoria_id`,
+    `habilitado`,
+    `estado_id`
+)
+VALUES(
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `ecomerce`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `bo_usuarios`
---
-
-CREATE TABLE `bo_usuarios` (
-  `bo_usuarios` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `tipo` int(11) NOT NULL,
-  `habilitado` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `categorias`
---
-
-CREATE TABLE `categorias` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `habilitado` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `categorias`
---
-
-INSERT INTO `categorias` (`id`, `nombre`, `habilitado`) VALUES
-(1, 'Televisores', 1),
-(2, 'Notebooks', 1),
-(3, 'Lavarropas', 1),
-(4, 'Consolas', 1),
-(6, 'Heladeras', 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `compras`
---
-
-CREATE TABLE `compras` (
-  `id` int(11) NOT NULL,
-  `monto` decimal(10,0) NOT NULL,
-  `fecha` datetime NOT NULL,
-  `producto_id` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `usuario_id` int(11) NOT NULL,
-  `estado_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `compras_estados`
---
-
-CREATE TABLE `compras_estados` (
-  `id` int(11) NOT NULL,
-  `estado` varchar(100) NOT NULL,
-  `habilitado` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `productos`
---
-
-CREATE TABLE `productos` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `descuento` varchar(50) NOT NULL,
-  `codigo` varchar(30) NOT NULL,
-  `precio` decimal(10,0) NOT NULL,
-  `precio_descuento` decimal(10,0) NOT NULL,
-  `categoria_id` int(11) NOT NULL,
-  `habilitado` tinyint(1) NOT NULL DEFAULT '1',
-  `estado_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `productos_estados`
---
-
-CREATE TABLE `productos_estados` (
-  `id` int(11) NOT NULL,
-  `estado` varchar(50) NOT NULL,
-  `habilitado` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `productos_estados`
---
-
-INSERT INTO `productos_estados` (`id`, `estado`, `habilitado`) VALUES
-(1, 'En Stock', 1),
-(2, 'Sin Stock', 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `habilitado` tinyint(1) DEFAULT '1',
-  `nombre` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `email`, `password`, `habilitado`, `nombre`) VALUES
-(1, 'usuario1@mail.com', 'password1', 1, 'andres'),
-(2, 'usuario2@mail.com', 'password2', 1, 'sofia'),
-(3, 'usuario3@mail.com', 'password3', 1, 'veronica'),
-(4, 'usuario4@mail.com', 'password4', 1, 'julio'),
-(5, 'usuario5@mail.com', 'password5', 1, 'alejandro');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `bo_usuarios`
---
-ALTER TABLE `bo_usuarios`
-  ADD PRIMARY KEY (`bo_usuarios`);
-
---
--- Indices de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `compras`
---
-ALTER TABLE `compras`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `estado_id` (`estado_id`);
-
---
--- Indices de la tabla `compras_estados`
---
-ALTER TABLE `compras_estados`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `categoria_id` (`categoria_id`),
-  ADD KEY `estado_id` (`estado_id`);
-
---
--- Indices de la tabla `productos_estados`
---
-ALTER TABLE `productos_estados`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT de la tabla `productos_estados`
---
-ALTER TABLE `productos_estados`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `compras`
---
-ALTER TABLE `compras`
-  ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`estado_id`) REFERENCES `compras_estados` (`id`);
-
---
--- Filtros para la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`),
-  ADD CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`estado_id`) REFERENCES `productos_estados` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+    'Sony Modelo 1',
+    '',
+    '',
+    '3000',
+    '',
+    '1',
+    '1',
+    ''
+),(
+    'Sony Modelo 2',
+    '',
+    '',
+    '4000',
+    '',
+    '1',
+    '1',
+    '1'
+),(
+    'Sony Modelo 3',
+    '',
+    '',
+    '5000',
+    '',
+    '1',
+    '1',
+    '1'
+),(
+    'Sony Modelo 4',
+    '',
+    '',
+    '6000',
+    '',
+    '1',
+    '1',
+    '1'
+),(
+    'Philips Modelo 1',
+    '',
+    '',
+    '4500',
+    '',
+    '1',
+    '1',
+    '1'
+),(
+    'Philips Modelo 2',
+    '',
+    '',
+    '3500',
+    '',
+    '1',
+    '1',
+    '1'
+),(
+    'Philips Modelo 3',
+    '',
+    '',
+    '2750',
+    '',
+    '1',
+    '1',
+    '1'
+),(
+    'Hitachi Modelo 1',
+    '',
+    '',
+    '4600',
+    '',
+    '1',
+    '1',
+    '1'
+),(
+    'Hitachi Modelo 2',
+    '',
+    '',
+    '6000',
+    '',
+    '1',
+    '1',
+    '1'
+),(
+    'Hitachi Modelo 3',
+    '',
+    '',
+    '1500',
+    '',
+    '1',
+    '1',
+    '1'
+)
